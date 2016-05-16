1c79322028b44a4afbb36fb0a9a16b24ef5a43d0
RS <schachte@RSs-MacBook-Pro.attlocal.net>
2016-05-15 21:14:55 -0500

    Finalized queried scraped data into frontend

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ var app             =         express();
 var bodyParser      =         require("body-parser");
 var Bing            = require('node-bing-api')({ accKey: "MAQhhIVshKb7wYs7KWB0s44VA3F4hV6RurQ69aU/3DI" });
 var prof_list;
+var prof_links;
 
 //Grabbing and parsing body data from AJAX request from client to server
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,13 +33,20 @@ function getProfs(teacher_name, successCallback) {
     }, function(error, res, body){
 
       //Store the results for the current search query
-      var teacher_results = [];
+      var teacher_results   = [];
+      var teacher_links     = [];
+
       try {
         for (var i in body.d.results) {
 
-          //Assign the data for the bing result title
+          //Assign the prof title to X
           var x = body.d.results[i].Title;
+          console.log("prof is " + x);
+          var y = body.d.results[i].Url;
+          console.log("url is " + y);
 
+
+          //Don't push results with the following keywords (BAD RESPONSE DATA)
           if (x.includes('Add') || x.includes('RATINGS') || x.includes("Hint")) {}
 
           else {
@@ -47,9 +55,9 @@ function getProfs(teacher_name, successCallback) {
             try{
               x = x.split(' at ');
               teacher_results.push(x[0] + " who works at " + x[1]);
+              teacher_links.push(y);
             }
             catch(err) {
-
               //If you can't split by the standard format, grab what you can
               try {
 
@@ -58,13 +66,14 @@ function getProfs(teacher_name, successCallback) {
                 else{
                   //Push the current prof data
                   teacher_results.push(x[0]);
+                  teacher_links.push(y);
                 }
               }
               catch(err) {}
             }
         }
       }
-        successCallback(teacher_results);
+        successCallback(teacher_results, teacher_links);
       }
       catch (err) {
         console.log(err);
@@ -94,10 +103,10 @@ exports.notFound = function(req, res) {
 exports.ajaxURL = function(req, res) {
 
   if (req.method == "GET"){
-    getProfs(req.query.search + " site:ratemyprofessors.com", function(data) {
-      prof_list = data;
-      res.send(prof_list);
-      res.send("LINKHERE");
+    getProfs(req.query.search + " site:ratemyprofessors.com", function(prof_data, link_data) {
+      prof_list = prof_data;
+      prof_links = link_data
+      res.json({ professors: prof_list, link_data: prof_links });
     });
   }
 }
@@ -105,39 +114,50 @@ exports.ajaxURL = function(req, res) {
 //Grab the data from ratemyprofessor
 exports.getProfRatingData = function(req, res) {
 
-  request("http://www.ratemyprofessors.com/ShowRatings.jsp?tid=1069834", function(err, resp, body) {
 
-    //If we could successfully grab the URL, begin scraping the data
-    if (!err && resp.statusCode == 200) {
+  function scrapeMyStuff(callback) {
 
-      //Get the body data of the ratemyprof
-      var $ = cheerio.load(body);
+      request(req.query.linker, function(err, resp, body) {
 
-      var rating_overall      = $('.grade')[0].children[0].data;
-      var rating_helpfulness  = $('.rating')[0].children[0].data;
-      var rating_clarity      = $('.rating')[1].children[0].data;
-      var rating_easiness     = $('.rating')[2].children[0].data;
+        //If we could successfully grab the URL, begin scraping the data
+        if (!err && resp.statusCode == 200) {
 
-      var completed_rating_object = {
-        "rating_data":[
-          {
-            "name"        : "professor_name",
-            "overall"     : rating_overall,
-            "helpfulness" : rating_helpfulness,
-            "clairty"     : rating_clarity,
-            "easiness"    : rating_easiness
-          }
-      ]
-    };
+          //Get the body data of the ratemyprof
+          var $ = cheerio.load(body);
 
-      console.log(completed_rating_object);
+          var rating_overall      = $('.grade')[0].children[0].data;
+          var rating_helpfulness  = $('.rating')[0].children[0].data;
+          var rating_clarity      = $('.rating')[1].children[0].data;
+          var rating_easiness     = $('.rating')[2].children[0].data;
 
+          var completed_rating_object = {
+
+            "rating_data":[
+              {
+                "name"        : req.query.professor,
+                "overall"     : rating_overall,
+                "helpfulness" : rating_helpfulness,
+                "clarity"     : rating_clarity,
+                "easiness"    : rating_easiness
+              }
+          ]
+        };
+
+          callback(completed_rating_object);
+
+        }
+
+      });
     }
-
-  });
 
 
   if (req.method == "GET") {
+
+    scrapeMyStuff(function(rating_data) {
+
+      res.json(rating_data);
+
+    });
 
 
   }
